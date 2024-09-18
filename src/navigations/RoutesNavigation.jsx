5582c69ba9300db952e0ae7cd8416ce762d95d0a
himanshu sinha <himanshusinha@himanshus-Mini.bbrouter>
2024-09-18 22:41:24 +0530

    latest

--- src/navigations/RoutesNavigation.jsx
+++ src/navigations/RoutesNavigation.jsx
@@ -1,11 +1,11 @@
 import React, {useEffect} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
-import {onAuthStateChanged} from 'firebase/auth';
-import {auth} from '../config/firebaseConfig';
+import {auth} from '../config/firebaseConfig'; // Ensure this exports the auth object correctly
 import {setUser, setUserLoading} from '../redux/slices/user';
 import AppNavigation from './AppNavigation';
 import AuthNavigation from './AuthNavigation';
 import {ActivityIndicator} from 'react-native';
+import {onAuthStateChanged} from 'firebase/auth';
 
 const RoutesNavigation = () => {
   const dispatch = useDispatch();
@@ -14,9 +14,9 @@ const RoutesNavigation = () => {
   useEffect(() => {
     dispatch(setUserLoading(true)); // Set loading to true
 
-    const unsubscribe = onAuthStateChanged(auth, u => {
-      if (u) {
-        const {uid, email, displayName = null} = u;
+    const unsubscribe = onAuthStateChanged(auth, user => {
+      if (user) {
+        const {uid, email, displayName = null} = user;
         dispatch(setUser({uid, email, displayName}));
       } else {
         dispatch(setUser(null)); // User is not authenticated
@@ -24,7 +24,7 @@ const RoutesNavigation = () => {
       dispatch(setUserLoading(false)); // Set loading to false
     });
 
-    return () => unsubscribe();
+    return () => unsubscribe(); // Clean up the subscription
   }, [dispatch]);
 
   // Optionally handle loading state if needed
